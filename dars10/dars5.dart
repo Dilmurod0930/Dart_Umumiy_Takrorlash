7b9c98c27aade8bb792664ed0d24f6368505ac92
Dilmurod09 <dilmurodqurbonov2002@gmail.com>
2022-09-12 21:22:40 +0500

    add

---
+++ dars10/dars5.dart
@@ -0,0 +1,12 @@
+//? AvtoSalon nomli class yaratilsin   Unda parametir sifatida
+//? Moshina  modeli,  yiili,   markasi ,  zavod nomi,  bo'lsin.
+//? Class maqsadi Foydalanuvchidan  o'zida bor summani qabull qilib 
+//? Foydalanuvchi kiritgan  summaga qaysi moshinalar  beradi shu,  
+//? moshinalar ro'yxatini chiqarsin. Foydalanuvchi  tanlasin qaysi moshinani
+//? tanlasa unga doir   malumotlarni barchasini chiqarsin.  Va yana
+//? foydlanivchi  so'ralsin bu moshina sizga yoqtimi yoki yo'qligini. 
+//? Agar yoqqan bo'lsa Savdoyingiz manfatli bo'ldi raxmat  moshinani olib ketishingiz
+//? mumkin deb chiqarsin agar yoqmagan bo'lsa  birinchi kiritgan puli yetadigan
+//? moshinalarni ro'yxatini yana ekranga  bostirsin boshqa moshinani tanlasin.
+//? Foydalanuvchiga yoqgan moshina chiqmaguncha shu holat  takrorlanaversin. 
+//? Yani yoqmasa  boshqatdan moshinalar ro'yxatiga qaytaraversin.
